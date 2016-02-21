cf0ba5b94eae16a69ea34bcc9a32ddf16c69b397
Tarun Atrey <tarunatrey@gmail.com>
2016-02-20 18:14:40 -0800

    Update to select Step-1 options
    Newly added code selects date, time and duration for reservation.

--- AutoReserve.py
+++ AutoReserve.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.keys import Keys
 
 strUsername = 'XYZ@gmail.com'
 strPassword = 'PASSWORD\n'
-strDate = '05/15/2015'
+strDate = '01/01/2016'
 strTime = '1:00 PM'
 strDuration = '1.5 hours' 
 
@@ -23,8 +23,23 @@ with Browser() as browser:
     browser.find_by_id('password').fill(strPassword)
     time.sleep(5)
     browser.visit('https://elan.activebuilding.com/portal/reservations/step1/?amenityId=1741')
+
+    #select date for reservation
     browser.find_by_id('startDate').fill(strDate)
     browser.find_by_id('content').click()
-    browser.find_by_tag('select').first.click()
+
+    #select time for reservation
+    el = browser.find_by_id('startTime')
+    for option in el.find_by_tag('option'):
+        if option.text == '1:00 PM':
+            option.click()
+            break
+
+    #select duration for reservation
+    el = browser.find_by_id('durationHours')
+    for option in el.find_by_tag('option'):
+        if option.text == '1.5 hours':
+            option.click()
+            break
     
     time.sleep(5)
